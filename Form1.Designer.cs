f5494d9b2da85e5207c4000722d04c4137dbad67
francismiguelnazareno11-del <francismiguelnazareno11@gmail.com>
2026-05-14 10:25:07 +0800

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace Login_Form
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtBusername = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtBpassword = new System.Windows.Forms.TextBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.label1.Location = new System.Drawing.Point(940, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(148, 77);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "LOGIN FORM";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtBusername
+            // 
+            this.txtBusername.Location = new System.Drawing.Point(879, 156);
+            this.txtBusername.Name = "txtBusername";
+            this.txtBusername.Size = new System.Drawing.Size(168, 20);
+            this.txtBusername.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.label2.Location = new System.Drawing.Point(876, 138);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(86, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Username:";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label3
+            // 
+            this.label3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.label3.Location = new System.Drawing.Point(876, 189);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(86, 15);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Password:";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // txtBpassword
+            // 
+            this.txtBpassword.Location = new System.Drawing.Point(879, 207);
+            this.txtBpassword.Name = "txtBpassword";
+            this.txtBpassword.Size = new System.Drawing.Size(168, 20);
+            this.txtBpassword.TabIndex = 3;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(848, 258);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(239, 36);
+            this.btnLogin.TabIndex = 5;
+            this.btnLogin.Text = "button1";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1283, 450);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtBpassword);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtBusername);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtBusername;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtBpassword;
+        private System.Windows.Forms.Button btnLogin;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using UserNameSpace;
+namespace Login_Form
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+       
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            
+            string inputPass = txtBpassword.Text;
+            string inputID = txtBusername.Text;
+
+            Administrator systemAdmin = new Administrator("Lebron", "admin123", "5678");
+
+            if (systemAdmin.verifyLogin(inputID, inputPass))
+            {
+                MessageBox.Show("NakaLOGIN KANA SAWAKAS.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                systemAdmin.updatePassword("BagongPassword123");
+                MessageBox.Show("Bagong password yarn?");
+
+                systemAdmin.updateAdminName("Curry");
+                MessageBox.Show("Bagong Name yarn?");
+
+            }
+            else
+            {
+                MessageBox.Show("Mali ka ng nilagay dodong");
+            }
+        }
+    }
+}
+
+namespace UserNameSpace
+{
+    public class User
+    {
+        private string user_id;
+        protected string user_password;
+
+        public User(string id, string pass)
+        {
+            this.user_id = id;
+            this.user_password = pass;
+        }
+
+        public bool verifyLogin(string id, string pass)
+        {
+            if (this.user_id.Equals(id) && this.user_password.Equals(pass))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private void updatePassword(string newPassword)
+        {
+
+            this.user_password = newPassword;
+        }
+    }
+
+
+    public class Administrator : User
+    {
+        private string admin_name;
+
+        public Administrator(string name, string id, string pass) : base(id, pass)
+        {
+            this.admin_name = name;
+
+
+        }
+
+        public void updatePassword(string newPassword)
+        {
+            this.user_password = newPassword;
+        }
+
+        public void updateAdminName(string name)
+        {
+            this.admin_name = name;
+        }
+
+    }
+
+
+}
